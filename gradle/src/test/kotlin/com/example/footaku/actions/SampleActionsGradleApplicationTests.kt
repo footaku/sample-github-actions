000d3e58a33dcada61ae1554471abd1f0d092db0
footaku <18702821+footaku@users.noreply.github.com>
2020-03-19 18:25:09 +0900

    init

---
+++ gradle/src/test/kotlin/com/example/footaku/actions/SampleActionsGradleApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.example.footaku.actions
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleActionsGradleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+		Assertions.assertTrue(true)
+	}
+
+}
